0a4b9f0537a2769f60548506f0a775d1ae24a8cf
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-11-19 14:40:38 +0000

    Rotas de login e da loja adicionadas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,17 +5,34 @@ import ServicePerucas from "../pages/ServicePerucas";
 import ServiceTrancas from "../pages/ServiceTrancas";
 import Serviceestetica from "../pages/ServiceEstetica";
 import Agendar from "../pages/Agendar";
+
+import Loja from "../pages/Loja";
+
+import Login from "../auth/Login";
+import CadastrarSe from "../auth/CadastrarSe";
+import RecuperarSenha from "../auth/RecuperarSenha";
+
 import NotFound from "../pages/NotFound";
 
 export default function AppRoutes () {
     return (
         <Routes>
+            {/*Rotas do site */}
             <Route path="/" element={<Home />} />
             <Route path="/servico/cabeleireiro" element={<ServiceHair />} />
             <Route path="/servico/extensoes-e-perucas" element={<ServicePerucas />} />
             <Route path="/servico/trancas-e-penteados" element={<ServiceTrancas />} />
             <Route path="/servico/estetica" element={<Serviceestetica />} />
             <Route path="/agendar" element={<Agendar />} />
+
+            {/*Rotas de loja */}
+            <Route path="/loja" element={<Loja />} />
+
+            {/*Rotas de login */}
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/cadastrar-se" element={<CadastrarSe />} />
+            <Route path="/auth/recuperar-senha" element={<RecuperarSenha />} />
+
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
