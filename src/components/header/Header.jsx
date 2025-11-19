47a7a35d36c9d0f8cdebdb83d3e0120d7bc8bf75
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-11-19 15:35:18 +0000

    Item galeria no menu atualizado

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,7 +17,7 @@ export default function Header() {
                 <a href="/">Home</a>
                 <a href="/#sobre">Sobre</a>
                 <a href="/#servicos">Serviços</a>
-                <a href="/#galeria">Galeria</a>
+                <a href="/#galeria">Galerias</a>
                 <a href="/agendar">Agendar</a>
                 <a href="/#fale-conosco">Fale Conosco</a>
             </nav>
